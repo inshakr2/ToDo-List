00ad0f38389a1bceb234753c9ebcdc8986baf91b
Chany <inshakr2@naver.com>
2023-02-23 20:24:31 +0900

    Initial commit

---
+++ src/test/java/com/moais/ToDoList/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moais.ToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
