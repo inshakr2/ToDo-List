785afb95005531fdb5f88e73261cedcae2aa46be
Chany <inshakr2@naver.com>
2023-02-24 15:41:45 +0900

    ToDoList - DB 연동

--- src/main/java/com/moais/ToDoList/controller/CommonController.java
+++ src/main/java/com/moais/ToDoList/controller/CommonController.java
@@ -1,6 +1,7 @@
 package com.moais.ToDoList.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +12,5 @@ public class CommonController {
 
         return "Hello Moais";
     }
-
 
 }
---
+++ src/main/java/com/moais/ToDoList/domain/Account.java
@@ -0,0 +1,38 @@
+package com.moais.ToDoList.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private String nickName;
+
+    private Account(String email, String password, String name, String nickName) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.nickName = nickName;
+    }
+
+    public Account joinUs(String email, String password, String name, String nickName) {
+
+        return new Account(email, password, name, nickName);
+
+    }
+}
