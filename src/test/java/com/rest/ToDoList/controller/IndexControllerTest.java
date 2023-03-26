0aaa8cba05db8d8fa3bdf746b8086effa1a2dc32
Chany <inshakr2@naver.com>
2023-03-26 23:36:07 +0900

    ToDoList - Test 코드 리팩터링

--- src/test/java/com/rest/ToDoList/controller/IndexControllerTest.java
+++ src/test/java/com/rest/ToDoList/controller/IndexControllerTest.java
@@ -1,29 +1,14 @@
 package com.rest.ToDoList.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.rest.ToDoList.common.RestDocConfiguration;
+import com.rest.ToDoList.common.BaseTestController;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
-@Import(RestDocConfiguration.class)
-@ActiveProfiles("test")
-public class IndexControllerTest {
 
-    @Autowired
-    MockMvc mockMvc;
+public class IndexControllerTest extends BaseTestController {
 
     @Test
     public void index() throws Exception {
--- src/test/java/com/rest/ToDoList/controller/ToDoControllerTests.java
+++ src/test/java/com/rest/ToDoList/controller/ToDoControllerTests.java
@@ -1,20 +1,12 @@
 package com.rest.ToDoList.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.rest.ToDoList.common.RestDocConfiguration;
+import com.rest.ToDoList.common.BaseTestController;
 import com.rest.ToDoList.domain.ToDo;
 import com.rest.ToDoList.dto.ToDoDto;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 
@@ -27,19 +19,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
-@Import(RestDocConfiguration.class)
-@ActiveProfiles("test")
-public class ToDoControllerTests {
-
-    @Autowired
-    MockMvc mockMvc;
-
-    @Autowired
-    ObjectMapper objectMapper;
-
+public class ToDoControllerTests extends BaseTestController {
 
     @Test
     public void createToDo() throws Exception {
