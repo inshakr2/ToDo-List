71a8854305a22869746c75711f5c6ba4b6880f0c
Chany <inshakr2@naver.com>
2023-04-26 00:41:11 +0900

    ToDoList - 테스트용 데이터

--- src/main/java/com/rest/ToDoList/config/AppConfig.java
+++ src/main/java/com/rest/ToDoList/config/AppConfig.java
@@ -1,9 +1,17 @@
 package com.rest.ToDoList.config;
 
+import com.rest.ToDoList.domain.Account;
+import com.rest.ToDoList.domain.AccountRole;
+import com.rest.ToDoList.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
 
 @Configuration
 public class AppConfig {
@@ -13,6 +21,23 @@ public class AppConfig {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    @Bean
+    public ApplicationRunner applicationRunner() {
 
+        return new ApplicationRunner() {
+
+            @Autowired
+            AccountService accountService;
+
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+
+                Account account = Account.join("chany@email.com", "chany",
+                                         Set.of(AccountRole.ADMIN, AccountRole.USER));
+                accountService.saveAccount(account);
+
+            }
+        };
+    }
 
 }
